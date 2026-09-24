d0d46395bbf037f3c6fdb61a6adb5eaf940b5578
tjarot <aprilia.mutiara557@smk.belajar.id>
2026-09-24 10:26:35 +0700

    chore(apk): add Capacitor Android + webDir dist

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vin.siswa',
+  appName: 'Siswa APK',
+  webDir: 'dist'
+};
+
+export default config;
